e2f049cbd58411873d5d7b50dc8bc234e654400c
rachfuerst <rach.fuerst@gmail.com>
2020-03-09 16:32:25 -0500

    Finished the Map methods

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function mapToNegativize(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(-1 * src[i])
+    }
+    return r
+  }
+
+  function mapToNoChange(array) {
+      let newArray = []
+
+      for(let i = 0; i < array.length; i++ ){
+          newArray.push(array[i])
+      }
+      return newArray
+  }
+
+  function mapToDouble(array) {
+      let newArray = []
+      for(let i=0;i < array.length; i++ ) {
+          newArray.push(2 * array[i])
+      }
+      return newArray
+  }
+
+  function mapToSquare(array) {
+      let newArray= []
+
+      for(let i =0 ; i< array.length; i++) {
+          newArray.push(array[i] * array[i])
+      }
+
+      return newArray
+  }
+
+  function reduceToTotal() {
+      
+  }
